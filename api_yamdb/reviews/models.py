10bb8f048c82a7beb94953d0b819f105178c3939
Дмитрий Кабанов <dvkabb@yandex.ru>
2023-07-25 21:37:12 +0300

    new models title category genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -28,3 +28,53 @@ class User(AbstractUser):
         ordering = ('id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.TextField(max_length=256)
+    year = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        # null=True,
+        related_name='titles'
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
