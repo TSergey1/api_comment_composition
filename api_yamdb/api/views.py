4e3b1360056398748d4e2db08704bb2782c2e66d
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2023-07-28 01:26:28 +0300

    1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-from rest_framework import status
 
 from api.serializers import GetTokenSerializer, RegistrationUserSerializer
 
