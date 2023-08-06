daaa622668e2e4571c34a2db4ad63ac2a0eac90d
Viktor <bigbagbom@yandex.ru>
2023-08-06 12:28:38 +0300

    Corrected review marks all

--- api_yamdb/api/management/commands/upload_csv.py
+++ api_yamdb/api/management/commands/upload_csv.py
@@ -9,58 +9,6 @@ from django.core.management.base import BaseCommand
 current_file = os.path.realpath(__file__)
 current_directory = os.path.dirname(current_file) + '/../../..'
 path_to_csv = current_directory + '/static/data/'
-uploads_info = {
-    'reviews_title': (
-        'titles.csv',
-        (
-            'id', 'name', 'year', 'category_id', 'description'
-        )
-    ),
-    'reviews_category': (
-        'category.csv',
-        (
-            'id', 'name', 'slug'
-        )
-    ),
-    'reviews_user': (
-        'users.csv',
-        (
-            'id',
-            'username',
-            'email',
-            'role',
-            'bio',
-            'first_name',
-            'last_name',
-            'password',
-            'last_login',
-            'is_superuser',
-            'is_staff',
-            'is_active',
-            'date_joined',
-        )
-    ),
-    'reviews_genre': (
-        'genre.csv', ('id', 'name', 'slug')
-    ),
-    'reviews_review': (
-        'review.csv',
-        (
-            'id', 'title_id', 'text', 'author_id', 'score', 'pub_date'
-        )
-    ),
-    'reviews_comment': (
-        'comments.csv',
-        (
-            'id', 'review_id', 'text', 'author_id', 'pub_date'
-        )
-    ),
-    'reviews_title_genre': (
-        'genre_title.csv', (
-            'id', 'title_id', 'genre_id'
-        )
-    ),
-}
 
 
 class Command(BaseCommand):
@@ -72,31 +20,41 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if options['upgrade_db']:
+            files = os.listdir(path_to_csv)
             conn = sqlite3.connect(current_directory + '/db.sqlite3')
-            for table, (csv_file, names) in uploads_info.items():
+            for csv_file in files:
+                if 'genre_' in csv_file:
+                    table = ('reviews_'
+                             + csv_file.split('_')[1].rstrip('s.csv')
+                             + '_' + csv_file.split('_')[0]
+                             )
+                else:
+                    table = 'reviews_' + csv_file.rstrip('s.csv')
                 conn.execute(f'''DELETE FROM {table};''')
-                path = path_to_csv + csv_file
+                cursor = conn.execute(f'select * from {table}')
+                names = list(map(lambda x: x[0], cursor.description))
                 data = pd.read_csv(
-                    path,
-                    header=None,
-                    names=names,
-                    skiprows=1,
+                    path_to_csv + csv_file,
+                    header=0,
                     keep_default_na=False
                 )
+                new_data = pd.DataFrame()
+                for n in names:
+                    regular = fr"^{n.split('_')[0]}.*$"
+                    if data.columns.str.contains(regular).any():
+                        new_data[n] = data.filter(regex=regular)
+                    else:
+                        new_data[n] = ''
+
                 if table == 'reviews_user':
-                    data = data.replace(
-                        {
-                            'is_active': {'': 1},
-                            'date_joined': {'': datetime.datetime.now()}
-                        }
-                    )
-                    for row in data.index:
-                        if data.loc[row, 'role'] == 'admin':
-                            data.loc[row, 'is_superuser'] = 1
-                            data.loc[row, 'is_staff'] = 1
-                        elif data.loc[row, 'role'] == 'moderator':
-                            data.loc[row, 'is_staff'] = 1
-                data.to_sql(table, conn, if_exists='append', index=False)
+                    new_data.loc[new_data['role']
+                                 == 'admin', ('is_superuser', 'is_staff')] = 1
+                    new_data.loc[new_data['role']
+                                 == 'moderator', 'is_staff'] = 1
+                    new_data.loc[
+                        new_data['id'] > 0, ('is_active', 'date_joined')
+                    ] = (1, datetime.datetime.now())
+                new_data.to_sql(table, conn, if_exists='append', index=False)
             conn.commit()
             conn.close()
         else:
